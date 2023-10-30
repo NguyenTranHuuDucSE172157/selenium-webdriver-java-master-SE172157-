af180a677e9364f1395ffe206aa750cb7f6a4772
huudu <huuduc20031501@gmail.com>
2023-10-30 15:46:29 +0700

    first commit

--- src/test/java/test/TC01.java
+++ src/test/java/test/TC01.java
@@ -1,14 +1,21 @@
 package test;
 import org.testng.annotations.Test;
 import driver.driverFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class TC01 {
     @Test
-    public void main() {
+    public void main() throws InterruptedException {
         
 
         // Create a WebDriver instance
@@ -26,14 +33,17 @@ public class TC01 {
             System.out.println("Title verification failed");
         }
 
+        Thread.sleep(2000);
         // Step 3: Click on MOBILE menu
         WebElement mobileMenu = driver.findElement(By.linkText("MOBILE"));
         mobileMenu.click();
 
+        Thread.sleep(2000);
         // Step 4: Select SORT BY dropdown as name
         Select sortDropdown = new Select(driver.findElement(By.cssSelector("select[title='Sort By']")));
         sortDropdown.selectByVisibleText("Name");
-
+        
+        Thread.sleep(2000);
         // Step 5: Verify all products are sorted by name
         java.util.List<WebElement> productNames = driver.findElements(By.cssSelector("h2.product-name a"));
         boolean isSorted = true;
@@ -49,6 +59,20 @@ public class TC01 {
             System.out.println("Products are not sorted by name");
         }
 
+        // Take screen shot of the result
+        File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        // Define the path where you want to save the screenshot
+        String screenshotPath = "D:/study FPT/Fall2023/SWT301/Assignment/Selenium/selenium-webdriver-java-master/src/test/java/images/TC01.png";
+
+        try {
+            // Copy the screenshot file to the desired location
+            FileUtils.copyFile(screenshotFile, new File(screenshotPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        Thread.sleep(2000);
         // Close the browser
         driver.quit();
     }
--- src/test/java/test/TC02.java
+++ src/test/java/test/TC02.java
@@ -2,20 +2,28 @@ package test;
 
 import org.testng.annotations.Test;
 import driver.driverFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class TC02 {
     @Test
-    public void main() {
+    public void main() throws InterruptedException {
 
         // Create a WebDriver instance
         WebDriver driver = driverFactory.getChromeDriver();
 
         // Step 1: get Link
         driver.get("http://live.techpanda.org/");
-
+        
+        Thread.sleep(2000);
         // Step bonus: Verify Title of the page
         String expectedTitle = "TechPanda";
         String actualTitle = driver.getTitle();
@@ -24,20 +32,41 @@ public class TC02 {
         } else {
             System.out.println("Verification failed");
         }
+
+        Thread.sleep(2000);
         // Step 2: Click on MOBILE menu
         WebElement mobileMenu = driver.findElement(By.linkText("MOBILE"));
         mobileMenu.click();
+
+        Thread.sleep(2000);
         // Step 3: Read the cost of Sony Xperia mobile
         String listPageCost = driver.findElement(By.xpath("//h2[contains(@class, 'product-name')]/a[text()='Sony Xperia']/parent::h2/following-sibling::div/span[@class='regular-price']/span[@class='price']")).getText();
-
+        
+        Thread.sleep(2000);
         // Step 4: Click on Sony Xperia mobile
         driver.findElement(By.linkText("SONY XPERIA")).click();
-
+        
+        Thread.sleep(2000);
          // Step 5: Read the Sony Xperia mobile cost from the detail page
          String detailPageCost = driver.findElement(By.cssSelector("span#product-price-1")).getText();
-
+         
+         Thread.sleep(2000);
          // Step 6: Compare the product cost in the list and details page
          assert listPageCost.equals(detailPageCost) : "Product cost mismatch";
+         
+         // Take screen shot of the result
+        File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 
+        // Define the path where you want to save the screenshot
+        String screenshotPath = "D:/study FPT/Fall2023/SWT301/Assignment/Selenium/selenium-webdriver-java-master/src/test/java/images/TC02.png";
+
+        try {
+            // Copy the screenshot file to the desired location
+            FileUtils.copyFile(screenshotFile, new File(screenshotPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Thread.sleep(2000);
+        driver.quit();
     }
 }
